5772203751e6cd1d915b11a732d77179371f20bb
Raymond <oetaray@gmail.com>
2023-06-22 16:37:55 +0300

    Completed assignments

---
+++ index.js
@@ -0,0 +1,18 @@
+function incrementing(number = 5){
+    number++;
+    number;
+    ++number
+    number;
+}
+function decrement(number){
+    number--;
+    number;
+    --number;
+    number;
+}
+function makeInt(number){
+    return parseInt(number, 5);
+}
+function preserveDeci(num = 5.5){
+    return parseFloat(num)
+}
